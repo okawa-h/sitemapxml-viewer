45fde8cb8e19efbfa14d3725024a928314c8f9fc
okawa-h <ookawa124@gmail.com>
2019-11-08 10:20:14 +0900

    update layout, jquery, style

--- files/js/script.js
+++ files/js/script.js
@@ -4,35 +4,34 @@
 
 	$(function() {
 
-		_$win  = $(window);
+		_$win = $(window);
 		_$tree = $('#tree');
-		// start();
-		
+
 		_$win.on({
 
-			dragenter:onDragenter,
-			dragover :onDragover,
-			dragleave:onDragleave,
-			drop     :onDrop
+			dragenter: onDragenter,
+			dragover : onDragover,
+			dragleave: onDragleave,
+			drop     : onDrop
 
 		});
 
 	});
 
 	function onDragenter(event) {
-		
+
 		cancel(event);
 
 	}
 
 	function onDragover(event) {
-		
+
 		cancel(event);
 
 	}
 
 	function onDragleave(event) {
-		
+
 		cancel(event);
 
 	}
@@ -40,20 +39,21 @@
 	function onDrop(event) {
 
 		cancel(event);
-		let file  = event.originalEvent.dataTransfer.files[0];
+    const file = event.originalEvent.dataTransfer.files[0];
+    const isXML = file.type === 'text/xml';
 
-		if (file.type != 'text/xml') {
+		if (!isXML) {
 
-			_$tree.empty().html('<p>File type is only [text/xml]</p>');
+			_$tree.empty().html('<p>The only file type that can be read is [text/xml].</p>');
 			return;
 
 		}
 
-		let reader = new FileReader();
+		const reader = new FileReader();
 		reader.onload = function(event) {
-			var xml = event.target.result;
-			var dom = new DOMParser().parseFromString(xml, 'text/xml');
-			onSuccess(dom);
+			const xml = event.target.result;
+			const dom = new DOMParser().parseFromString(xml, 'text/xml');
+			loadXML(dom);
 		};
 		reader.readAsText(file);
 
@@ -66,76 +66,58 @@
 
 	}
 
-	function start() {
+	function loadXML(xml) {
 
-		$.ajax({
-			url     :'./sitemap.xml',
-			type    :'GET',
-			dataType:'xml'
-		})
-		.then((...args) => {
+		const $xml = $(xml);
+		const $urls = $xml.find('url');
+		const location = $urls.eq(0).find('loc').text();
+		let map = new Object();
 
-			const [data, textStatus, jqXHR] = args;
-			onSuccess(data);
+		for (let i = 0; i < $urls.length; i++) {
 
-		})
-		.catch((...args) => {
+			const url = $urls.eq(i).find('loc').text().replace(location, '/');
+			const array = url.split('/');
+			const parentDir = array[1];
 
-			const [jqXHR, textStatus, errorThrown] = args;
-			console.log('fail', jqXHR.status);
-
-		});
-
-	}
-
-	function onSuccess(xml) {
-
-		const $document = $(xml);
-		const $urlList  = $document.find('url');
-		const location  = $urlList.eq(0).find('loc').text();
-		let map         = new Object();
-
-		for (let i = 0; i < $urlList.length; i++) {
-
-			let url      = $urlList.eq(i).find('loc').text().replace(location,'/');
-			let array    = url.split('/');
-			let category = array[1];
-
-			if (!map.hasOwnProperty(category)) map[category] = [];
-			map[category].push(url);
+			if (!map.hasOwnProperty(parentDir)) map[parentDir] = [];
+			map[parentDir].push(url);
 
 		}
 
-		setHtml(map,location);
+		setHtml(map, location);
 
 	}
 
-	function setHtml(map,location) {
+	function setHtml(map, location) {
 
-		let html = '<h2 class="site-url">' + location + '</h2>';
-		let keys = Object.keys(map);
+    let html = '<h2 class="site-url">URL : ' + location + '</h2>';
+    let totalLength = 0;
+    const keys = Object.keys(map);
+    keys.sort();
 
 		html += '<div class="directory-wrap">';
 		for (let i = 0; i < keys.length; i++) {
 
-			let key     = keys[i];
-			let urls    = map[key];
+			let key = keys[i];
+			let urls = map[key];
 
 			if (key == '' || key.length <= 0) continue;
 
 			urls.sort();
+      html += '<section class="directory" data-js="contenthidder">';
 
-			html += '<section class="directory" data-js="contenthidder">';
-			if (0 < urls.length) {
+			if (urls.length > 0) {
 
-				let data = getFilelistData(key,urls,location);
+				const data = getFilelistData(key, urls, location);
 
 				html += '<div class="info" data-contenthidder="toggle">';
-				html += '<h3>' + key + '</h3>';
-				html += '<p class="pagelength">' + data.length + 'ページ</p>';
-				html += '<button class="toggle">&nbsp</button>';
+          html += '<h3>' + key + '</h3>';
+          html += '<p class="pagelength">' + data.length + ' page</p>';
+          html += '<button class="toggle">&nbsp</button>';
 				html += '</div>';
-				html += data.html;
+        html += data.html;
+
+        totalLength += data.length;
 
 			} else {
 
@@ -146,7 +128,8 @@
 			html += '</section>';
 
 		}
-		html += '<div>';
+    html += '</div>';
+    html += '<p class="total">total : ' + totalLength + ' page</p>';
 
 		_$tree.empty().html(html);
 		setContentHidder();
@@ -161,16 +144,19 @@
 		for (let i = 0; i < urls.length; i++) {
 
 			const url = urls[i];
-			if (-1 < url.indexOf('.pdf') || -1 < url.indexOf('.xlsx')) continue;
+			if (url.indexOf('.pdf') > -1 || url.indexOf('.xlsx') > -1) continue;
 
-			const pageUrl = decodeURI(url.split('/' + key)[1]);
-			const href    = location + url;
+			const pageUrl = decodeURIComponent(url.split('/' + key)[1]);
+			const href = location + url;
 			counter++;
 			html += '<li><a href="' + href + '" target="_blank">' + pageUrl + '</a></li>';
 
 		}
 
-		return { 'length':counter,'html':html + '</ul>' };
+		return {
+      'length': counter,
+      'html': html + '</ul>'
+    };
 
 	}
 
@@ -186,11 +172,15 @@
 	let ContentHidder = function($parent) {
 
 		const $content = $parent.find('[data-contenthidder="content"]');
-		const $toggle  = $parent.find('[data-contenthidder="toggle"]');
+    const $toggle  = $parent.find('[data-contenthidder="toggle"]');
 
-		$toggle.on('click',function() {
-			$parent.toggleClass('active');
-		});
+    function onClick() {
+
+      $parent.toggleClass('active');
+
+    }
+
+		$toggle.on('click', onClick);
 
 	}
 
